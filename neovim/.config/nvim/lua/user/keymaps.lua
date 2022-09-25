068999514e105d60ca35f436ae4a8dec46456656
Hugo Pungs <dfxjnl@gmail.com>
2022-09-25 00:35:25 -0300

    neovim: update resize keymap

--- neovim/.config/nvim/lua/user/keymaps.lua
+++ neovim/.config/nvim/lua/user/keymaps.lua
@@ -24,8 +24,8 @@ vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", opts)
 vim.api.nvim_set_keymap("n", "<Leader>e", ":Lex 30<CR>", opts)
 
 -- Resize with arrows.
-vim.api.nvim_set_keymap("n", "<C-Up>", ":resize -2<CR>", opts)
-vim.api.nvim_set_keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+vim.api.nvim_set_keymap("n", "<C-Up>", ":resize +2<CR>", opts)
+vim.api.nvim_set_keymap("n", "<C-Down>", ":resize -2<CR>", opts)
 vim.api.nvim_set_keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 vim.api.nvim_set_keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
