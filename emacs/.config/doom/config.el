4a5abbc3a5ecdac8eca8224a717e79b6a45202ff
Hugo Pungs <dfxjnl@gmail.com>
2022-10-17 09:26:33 -0300

    chore(emacs): update

--- emacs/.config/doom/config.el
+++ emacs/.config/doom/config.el
@@ -5,34 +5,64 @@
 
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
-;; clients, file templates and snippets.
-(setq user-full-name "Hugo Pungs"
-      user-mail-address "dfxjnl@gmail.com")
+;; clients, file templates and snippets. It is optional.
+(setq
+  user-full-name
+  "Hugo Pungs"
+  user-mail-address "dfxjnl@gmail.com")
 
-;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
-;; are the three important ones:
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
-;; + `doom-font'
-;; + `doom-variable-pitch-font'
-;; + `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
 ;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
 ;;
-;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
-;; font string. You generally only need these two:
-;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
-;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
 
-;; If you use `org' and don't want your org files in the default location below,
-;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/projects/org/")
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+(setq
+  doom-theme
+  'doom-dracula
+  doom-font (font-spec :family "JetBrainsMono" :size 12)
+  doom-variable-pitch-font (font-spec :familty "Noto Sans" :size 13))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
 
-(setq confirm-kill-emacs nil)
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/projects/org/")
 
-;; Here are some additional functions/macros that could help you configure Doom:
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
 ;;
 ;; - `load!' for loading external *.el files relative to this one
 ;; - `use-package!' for configuring packages
@@ -45,168 +75,111 @@
 ;; To get information about any of these functions/macros, move the cursor over
 ;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
 ;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-(setq undo-limit (* 64 1024 1024)
-      evil-want-fine-undo t
-      password-cache-expiry nil
-      scroll-margin 2)
+;; Better defaults.
+(setq-default
+  window-combination-resize
+  t
+  x-stretch-cursor t)
 
-(global-subword-mode 1)
+(setq
+  truncate-string-ellipsis
+  "…"
+  password-cache-expiry nil
+  scroll-margin 2)
 
-(setq evil-split-window-below t
-      evil-vsplit-window-right t)
+(global-subword-mode 1) ; Iterate through CamelCase words.
 
-(setq doom-font (font-spec :family "JetBrains Mono" :size 12)
-      doom-big-font (font-spec :family "JetBrains Mono" :size 16))
+;; Windows.
+(setq
+  evil-split-window-below
+  t
+  evil-vsplit-window-right t)
 
-(setq doom-theme 'doom-dracula)
-
+;; Which-key.
 (setq which-key-idle-delay 0.5)
-
+;; Remove `evil-' appearing everywhere.
 (setq which-key-allow-multiple-replacements t)
-(after! which-key
+(after!
+  which-key
   (pushnew!
-   which-key-replacement-alist
-   '(("" . "\\`+?evil[-:]?\\(?:a-\\)?\\(.*\\)") . (nil . "◂\\1"))
-   '(("\\`g s" . "\\`evilem--?motion-\\(.*\\)") . (nil . "◃\\1"))))
+    which-key-replacement-alist
+    '(("" . "\\`+?evil[-:]?\\(?:a-\\)?\\(.*\\)") . (nil . "◂\\1"))
+    '(("\\`g s" . "\\`evilem--?motion-\\(.*\\)") . (nil . "◃\\1"))))
 
-(after! evil
-  (setq evil-ex-substitute-global t
-        evil-move-cursor-back nil
-        evil-kill-on-visual-paste nil))
+;; View large files.
+(use-package!
+  vlf-setup
+  :defer-incrementally
+  vlf-tune
+  vlf-base
+  vlf-write
+  vlf-search
+  vlf-occur
+  vlf-follow
+  vlf-ediff
+  vlf)
 
-(after! company
-  (setq company-idle-delay 0.5))
+;; Evil.
+(after!
+  evil
+  (setq
+    evil-ex-substitute-global
+    t
+    evil-move-cursor-back nil))
 
-(setq-default history-length 1000)
+;; Company.
+(after! company (setq company-idle-delay 0.5))
 
+;; Plain text.
 (set-company-backend!
   '(text-mode markdown-mode gfm-mode)
   '(:separate company-ispell company-files company-yasnippet))
 
-(setq projectile-ignored-projects '("~/" "/tmp" "~/.config/emacs/.local/straight/repos/"))
-(defun projectile-ignored-project-function (filepath)
-  "Return t if FILEPATH is within any of `projectile-ignored-projects'"
-  (or (mapcar (lambda (p) (s-starts-with-p p filepath)) projectile-ignored-projects)))
-
+;; YASnippet.
 (setq yas-triggers-in-field t)
 
-(use-package! string-inflection
-  :commands (string-inflection-all-cycle
-             string-inflection-toggle
-             string-inflection-camelcase
-             string-inflection-lower-camelcase
-             string-inflection-kebab-case
-             string-inflection-underscore
-             string-inflection-capital-underscore
-             string-inflection-upcase)
-  :init
-  (map! :leader :prefix ("c~" . "naming convention")
-        :desc "cycle" "~" #'string-inflection-all-cycle
-        :desc "toggle" "t" #'string-inflection-toggle
-        :desc "CamelCase" "c" #'string-inflection-camelcase
-        :desc "downCase" "d" #'string-inflection-lower-camelcase
-        :desc "kebab-case" "k" #'string-inflection-kebab-case
-        :desc "under_score" "_" #'string-inflection-underscore
-        :desc "Upper_Score" "u" #'string-inflection-capital-underscore
-        :desc "UP_CASE" "U" #'string-inflection-upcase)
-  (after! evil
-    (evil-define-operator evil-operator-string-inflection (beg end _type)
-      "Define a new evil operator that cycles symbol casing."
-      :move-point nil
-      (interactive "<R>")
-      (string-inflection-all-cycle)
-      (setq evil-repeat-info '([?g ?~])))
-    (define-key evil-normal-state-map (kbd "g~") 'evil-operator-string-inflection)))
+;; Emacs lisp auto-formatter.
+(use-package! elisp-autofmt :hook (emacs-lisp-mode . elisp-autofmt-mode))
+(add-hook!
+  'emacs-lisp-mode
+  (lambda ()
+    (require 'elisp-autofmt)
+    (elisp-autofmt-save-hook-for-this-buffer)))
 
-(after! marginalia
-  (setq marginalia-censor-variables nil)
-
-  (defadvice! +marginalia--anotate-local-file-colorful (cand)
-    "Just a more colourful version of `marginalia--anotate-local-file'."
-    :override #'marginalia--annotate-local-file
-    (when-let (attrs (file-attributes (substitute-in-file-name
-                                       (marginalia--full-candidate cand))
-                                      'integer))
-      (marginalia--fields
-       ((marginalia--file-owner attrs)
-        :width 12 :face 'marginalia-file-owner)
-       ((marginalia--file-modes attrs))
-       ((+marginalia-file-size-colorful (file-attribute-size attrs))
-        :width 7)
-       ((+marginalia--time-colorful (file-attribute-modification-time attrs))
-        :width 12))))
-
-  (defun +marginalia--time-colorful (time)
-    (let* ((seconds (float-time (time-subtract (current-time) time)))
-           (color (doom-blend
-                   (face-attribute 'marginalia-date :foreground nil t)
-                   (face-attribute 'marginalia-documentation :foreground nil t)
-                   (/ 1.0 (log (+ 3 (/ (+ 1 seconds) 345600.0)))))))
-      ;; 1 - log(3 + 1/(days + 1)) % grey
-      (propertize (marginalia--time time) 'face (list :foreground color))))
-
-  (defun +marginalia-file-size-colorful (size)
-    (let* ((size-index (/ (log10 (+ 1 size)) 7.0))
-           (color (if (< size-index 10000000) ; 10m
-                      (doom-blend 'orange 'green size-index)
-                    (doom-blend 'red 'orange (- size-index 1)))))
-      (propertize (file-size-human-readable size) 'face (list :foreground color)))))
-
-(use-package! page-break-lines
-  :commands page-break-lines-mode
-  :init
-  (autoload 'turn-on-page-break-lines-mode "page-break-lines")
-  :config
-  (setq page-break-lines-max-width fill-column)
-  (map! :prefix "g"
-        :desc "Prev page break" :nv "[" #'backward-page
-        :desc "Next page break" :nv "]" #'forward-page))
-
-(define-generic-mode sxhkd-mode
-  '(?#)
-  '("alt" "Escape" "super" "bspc" "ctrl" "space" "shift") nil
-  '("sxhkdrc") nil
-  "Simple mode for sxhkdrc files.")
-
-(setq lsp-clients-clangd-args '("--all-scopes-completion"
-                                "--background-index"
-                                "--clang-tidy"
-                                "--completion-style=detailed"
-                                "--header-insertion=iwyu"
-                                "--header-insertion-decorators"
-                                "-j=13"
-                                "--pch-storage=disk"))
+(setq lsp-clients-clangd-args '("--clang-tidy" "--header-insertion-decorators"))
 (after! lsp-clangd (set-lsp-priority! 'clangd 2))
 
 (defvar-local my/flycheck-local-cache nil)
-
 (defun my/flycheck-checker-get (fn checker property)
-  (or (alist-get property (alist-get checker my/flycheck-local-cache))
-      (funcall fn checker property)))
+  (or
+    (alist-get property (alist-get checker my/flycheck-local-cache))
+    (funcall fn checker property)))
 
 (advice-add 'flycheck-checker-get :around 'my/flycheck-checker-get)
 
 (add-hook 'lsp-managed-mode-hook
-          (lambda ()
-            (when (derived-mode-p 'c++-mode)
-              (setq my/flycheck-local-cache '((lsp . ((next-checkers . (c/c++-cppcheck)))))))))
-
-(use-package! highlight-escape-sequences
-  :hook (prog-mode . hes-mode))
-
-(setq flycheck-cppcheck-checks
-      '("style" "performance" "warning" "portability"))
+  (lambda ()
+    (when (derived-mode-p 'c++-mode)
+      (setq my/flycheck-local-cache
+        '((lsp . ((next-checkers . (c/c++-cppcheck)))))))))
+(setq flycheck-cppcheck-checks '("style" "performance" "warning" "portability"))
 
 (setq flycheck-checker-error-threshold nil)
 
-(add-hook! 'org-mode-hook #'solaire-mode)
+(use-package! highlight-escape-sequences :hook (prog-mode . hes-mode))
 
-(setq lsp-clients-lua-language-server-bin "/home/hugo/.local/bin/lua-language-server"
-      lsp-clients-lua-lsp-server-install-dir "/home/hugo/git/sumneko_lua"
-      lsp-clients-lua-language-server-main-location "/home/hugo/git/sumneko_lua/bin/main.lua")
-
-(setq-hook! 'lua-mode-hook +format-with-lsp nil)
+;; Language customizations.
+(define-generic-mode
+  sxhkd-mode
+  '(?#)
+  '("alt" "Escape" "super" "bspc" "ctrl" "space" "shift")
+  nil
+  '("sxhkd")
+  nil
+  "Simple mode for sxhkdrc files.")
