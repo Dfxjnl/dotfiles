d6a5c4231b6dcaca78d449acac892e52a48912ff
Hugo Pungs <dfxjnl@gmail.com>
2022-10-19 18:34:10 -0300

    chore: update doom

--- emacs/.config/doom/init.el
+++ emacs/.config/doom/init.el
@@ -45,25 +45,25 @@
   ophints                       ; highlight the region an operation acts on
   (popup +defaults)             ; tame sudden yet inevitable temporary windows
   ;;tabs                        ; a tab bar for Emacs
-  (treemacs +lsp)               ; a project drawer, like neotree but cooler
+  ;;(treemacs +lsp)             ; a project drawer, like neotree but cooler
   ;;unicode                     ; extended unicode support for various languages
   (vc-gutter +diff-hl +pretty)  ; vcs diff in the fringe
   vi-tilde-fringe               ; fringe tildes to mark beyond EOB
-  (window-select +numbers)      ; visually switch windows
+  ;;(window-select +numbers)    ; visually switch windows
   workspaces                    ; tab emulation, persistence & separate workspaces
   ;;zen                         ; distraction-free coding or writing
 
   :editor
   (evil +everywhere)            ; come to the dark side, we have cookies
   file-templates                ; auto-snippets for empty files
-  fold                          ; (nigh) universal code folding
+  ;;fold                        ; (nigh) universal code folding
   format                        ; automated prettiness
   ;;god                         ; run Emacs commands without modifier keys
   ;;lispy                       ; vim for lisp, for people who don't like vim
-  multiple-cursors              ; editing in many places at once
+  ;;multiple-cursors            ; editing in many places at once
   ;;objed                       ; text object editing for the innocent
   ;;parinfer                    ; turn lisp into python, sort of
-  rotate-text                   ; cycle region at point between text candidates
+  ;;rotate-text                 ; cycle region at point between text candidates
   snippets                      ; my elves. They type so I don't have to
   ;;word-wrap                   ; soft wrapping with language-aware indent
 
@@ -78,7 +78,7 @@
   ;;eshell                      ; the elisp shell that works everywhere
   ;;shell                       ; simple shell REPL for Emacs
   ;;term                        ; basic terminal emulator for Emacs
-  vterm                         ; the best terminal emulation in Emacs
+  ;;vterm                       ; the best terminal emulation in Emacs
 
   :checkers
   (syntax +childframe)          ; tasing you for every semicolon you forget
@@ -127,7 +127,7 @@
   ;;dhall
   ;;elixir                      ; erlang done right
   ;;elm                         ; care for a cup of TEA?
-  (emacs-lisp +tree-sitter)     ; drown in parentheses
+  emacs-lisp                    ; drown in parentheses
   ;;erlang                      ; an elegant language for a more civilized age
   ;;ess                         ; emacs speaks statistics
   ;;factor
@@ -149,12 +149,12 @@
   ;;latex                       ; writing papers in Emacs has never been so fun
   ;;lean                        ; for folks with too much to prove
   ;;ledger                      ; be audit you can be
-  (lua +lsp +tree-sitter)       ; one-based indices? one-based indices
-  (markdown +tree-sitter)       ; writing docs for people to ignore
+  (lua +lsp)                    ; one-based indices? one-based indices
+  markdown                      ; writing docs for people to ignore
   ;;nim                         ; python + lisp at the speed of c
   ;;nix                         ; I hereby declare "nix geht mehr!"
   ;;ocaml                       ; an objective camel
-  (org +pretty +tree-sitter)    ; organize your plain life in plain text
+  (org +pretty)                 ; organize your plain life in plain text
   ;;php                         ; perl's insecure younger brother
   ;;plantuml                    ; diagrams for confusing people more
   ;;purescript                  ; javascript, but functional
@@ -185,7 +185,7 @@
   :app
   ;;calendar
   ;;emms
-  ;;everywhere                  ; *leave* Emacs!? You must be joking
+  everywhere                    ; *leave* Emacs!? You must be joking
   ;;irc                         ; how neckbeards socialize
   ;;(rss +org)                  ; emacs as an RSS reader
   ;;twitter                     ; twitter client https://twitter.com/vnought
