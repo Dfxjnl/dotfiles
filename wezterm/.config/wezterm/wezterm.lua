4d15f78bf445ef46383bfb80c7a7c9ff19785975
Dfx Jnl <dfxjnl@gmail.com>
2024-02-21 18:22:14 -0300

    Update WezTerm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,13 +1,25 @@
 local wezterm = require("wezterm")
 
-local config = {
-	adjust_window_size_when_changing_font_size = false,
-	color_scheme = "Catppuccin Frappe",
-	font = wezterm.font("JetBrainsMono Nerd Font"),
-	force_reverse_video_cursor = true,
-	hide_tab_bar_if_only_one_tab = true,
-	window_close_confirmation = "NeverPrompt",
-	window_decorations = "RESIZE",
+local padding = 10
+local config = {}
+
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.adjust_window_size_when_changing_font_size = false
+config.check_for_updates = false
+config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.window_close_confirmation = "NeverPrompt"
+config.window_padding = {
+	left = padding,
+	right = padding,
+	top = padding,
+	bottom = padding,
 }
 
 return config
