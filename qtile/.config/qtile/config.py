80bee0360eb760d024265d74cff12cb4be266853
Dfx Jnl <dfxjnl@gmail.com>
2024-02-21 20:43:23 -0300

    Improve QTile's bar

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -139,7 +139,7 @@ layouts = [
 ]
 
 widget_defaults = dict(
-    font="sans",
+    font="Noto Sans",
     fontsize=12,
     padding=3,
 )
@@ -159,15 +159,13 @@ screens = [
                     },
                     name_transform=lambda name: name.upper(),
                 ),
-                widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
                 # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
                 # widget.StatusNotifier(),
                 widget.Systray(),
-                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-                widget.QuickExit(),
+                widget.Clock(format="%H:%M | %d-%m-%Y"),
             ],
             24,
+            background="#1E1E2E",
             # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
             # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
