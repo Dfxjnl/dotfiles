93d88c5fdf652a2f348f87dcd8905f234072559e
Dfx Jnl <dfxjnl@gmail.com>
2025-01-22 20:17:45 -0300

    Update neovim

--- neovim/.config/nvim/lua/plugins/editor.lua
+++ neovim/.config/nvim/lua/plugins/editor.lua
@@ -2,12 +2,7 @@ return {
   {
     "max397574/better-escape.nvim",
     config = function()
-      require("better_escape").setup({
-        clear_empy_lines = true,
-        keys = function()
-          return vim.api.nvim_win_get_cursor(0)[2] > 1 and "<esc>l" or "<esc>"
-        end,
-      })
+      require("better_escape").setup()
     end,
   },
 }
