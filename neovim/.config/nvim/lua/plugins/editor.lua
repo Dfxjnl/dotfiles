f0aea3bfede6ae437dd2737029b459fb9af30455
Dfx Jnl <dfxjnl@gmail.com>
2025-04-29 00:11:42 -0300

    Update neovim's snack picker to show hidden files

--- neovim/.config/nvim/lua/plugins/editor.lua
+++ neovim/.config/nvim/lua/plugins/editor.lua
@@ -1,4 +1,16 @@
 return {
+  "folke/snacks.nvim",
+  opts = {
+    -- show hidden files in snacks.explorer
+    picker = {
+      sources = {
+        explorer = {
+          -- show hidden files like .env
+          hidden = true,
+        },
+      },
+    },
+  },
   {
     "max397574/better-escape.nvim",
     config = function()
