168798d7f4d0f92e00adef3b8cc539447e4ad614
Dfx Jnl <dfxjnl@gmail.com>
2025-09-28 07:39:51 -0300

    neovim: Bump

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,17 +5,4 @@ return {
       colorscheme = "catppuccin",
     },
   },
-  -- Temporary fix for catppuccin incompability
-  {
-    "catppuccin/nvim",
-    opts = function(_, opts)
-      local module = require("catppuccin.groups.integrations.bufferline")
-
-      if module then
-        module.get = module.get_theme
-      end
-
-      return opts
-    end,
-  },
 }
